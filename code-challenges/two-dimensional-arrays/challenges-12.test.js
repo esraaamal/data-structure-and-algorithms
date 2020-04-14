ff80705a4459e4dcf58c2bad126ce54a566843b2
esraaamal <ma.esraa@hotmail.com>
2020-04-14 08:41:40 +0300

    f

--- code-challenges/two-dimensional-arrays/challenges-12.test.js
+++ code-challenges/two-dimensional-arrays/challenges-12.test.js
@@ -47,15 +47,30 @@ arr.push(total);
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 2
 
-Pat has decided that he would also like to organize his data as objects containing the number of cookies sold per hour and the time.
+Pat has decided that he would also like to organize his data as
+ objects containing the number of cookies sold per hour and the time.
 
 Here is sample data for the 9:00 sales: { sales: '88 cookies', time: '9 a.m.' }.
 
-Write a function named salesData that uses forEach to iterate over the hourlySales array and create an object for each hour. Return an array of the formatted data.
+Write a function named salesData that uses 
+forEach to iterate over the hourlySales array
+ and create an object for each hour. Return an array of the formatted data.
 ------------------------------------------------------------------------------------------------ */
 
 const salesData = (hours, data) => {
   // Solution code here...
+  let arr=[];
+  var valBook ={
+    sales: Number,
+    time:Number
+  }
+ let nweArr =data.forEach((val,idx) =>{
+    valBook.sales = `${val} cookies`;
+    valBook.time = hours[idx];
+    arr.push(valBook);
+
+ })
+ return arr;
 };
 
 /* ------------------------------------------------------------------------------------------------
